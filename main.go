d2d6876e2f0f2cae3910374ea51ada25bd89dc61
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2025-08-25 11:29:13 -0300

    Configurando primeiro endpoint para testes

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
+
+	server.Run(":5059") //localhost:5059
+
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "HELLO!"})
 }
