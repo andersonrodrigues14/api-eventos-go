d7caa2d2d1e092077f195837989c9185c8152ead
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2025-08-25 13:19:51 -0300

    Adicionada validações de campos obrigatórios e criado evento de criação

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/andersonrodrigues14/api-eventos-go/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,10 +12,27 @@ func main() {
 
 	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
 
+	server.POST("/events", createEvent)
+
 	server.Run(":5059") //localhost:5059
 
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "HELLO!"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could nor parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created! ", "event": event})
 }
